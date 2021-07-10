559817a4e26d12bf51b71d16bc4490e58f8f6ff4
Marcelo-Diament <marcelodiament@gmail.com>
2021-07-10 16:54:10 -0300

    feature: request path parsed

--- index.js
+++ index.js
@@ -4,11 +4,25 @@
  */
 
 // Dependencies
-const http = require('http')
+const http = require('http'),
+  url = require('url')
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
+
+  // Get URL and parse it
+  const parsedUrl = url.parse(req.url, true)
+
+  // Get the path
+  const path = parsedUrl.pathname,
+    trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // Send the response
   res.end('HTTP Server Response\n')
+
+  // Log the request path
+  console.log(`Request received on path: ${trimmedPath}`)
+
 })
 
 // Start the server, and have it listen on port 3000
